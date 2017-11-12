bd54b3cc093e02c558046182ff6f0bc94353292c
Siorjik <serjkaplun@gmail.com>
2017-11-12 19:00:35 +0200

    fix save btn

--- src/components/SaveFriendBtn.js
+++ src/components/SaveFriendBtn.js
@@ -1,15 +1,10 @@
 import React, {Component} from 'react';
 
 export default class SaveFriendBtn extends Component {
-  save(ev) {
-    ev.preventDefault();
-    this.props.saveData();
-  }
-
   render() {
     let btn;
     if(!this.props.edit && !this.props.addNew) btn = null;
-    else btn = (<button className='save-btn' onClick={this.save.bind(this)}>Save</button>);
+    else btn = (<button className='save-btn'>Save</button>);
 
     return (<span>{btn}</span>);
   }
