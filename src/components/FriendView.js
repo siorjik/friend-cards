e623c5f350fda23f4729fe530eeebdab9ae6bb25
Siorjik <serjkaplun@gmail.com>
2017-11-18 21:25:56 +0200

    add view friend (routing)

---
+++ src/components/FriendView.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './css/FriendView.css';
+import {connect} from "react-redux";
+import {NavLink} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+
+class FriendView extends Component {
+  render() {
+    return (
+      <div>
+        <div className='friend-view'>
+          <h2><span><i>Name</i>:</span> {this.props.friend.name}</h2>
+          <h3><span><i>Age</i>:</span> {this.props.friend.age}</h3>
+          <h3><span><i>Phone</i>:</span> {this.props.friend.phone}</h3>
+          <NavLink to="/"><button className='view-back'>Back</button></NavLink>
+        </div>
+      </div>
+    );
+  }
+}
+
+let mapStateToProps = (state, ownProps)=>{
+  console.log(ownProps);
+  return {
+    friend: state.getFriends.find((friend)=>friend.id === +ownProps.match.params.id)
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(FriendView));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
 import getFriends from "./friends";
+import {routerReducer} from 'react-router-redux';
 
 let rootReducer = combineReducers({
+  routing: routerReducer,
   getFriends
 });
 
